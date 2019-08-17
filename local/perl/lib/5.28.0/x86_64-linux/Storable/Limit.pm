7856e8026e829e97d90f780b7ab392e7d42f4605
hukuhuku <hukuhuku11111arm52@gmail.com>
2019-08-17 23:35:39 +0900

    [add]firstcommit

---
+++ local/perl/lib/5.28.0/x86_64-linux/Storable/Limit.pm
@@ -0,0 +1,6 @@
+# bisected by stacksize
+$Storable::recursion_limit = 13642
+  unless defined $Storable::recursion_limit;
+$Storable::recursion_limit_hash = 8178
+  unless defined $Storable::recursion_limit_hash;
+1;
